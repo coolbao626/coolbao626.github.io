1a285ae6b3395978ed5ca823452724f34baed284
Ian Chen <ychen.desl@gmail.com>
2024-11-22 16:11:36 +0800

    feat: add product dialog

--- official-web/script.js
+++ official-web/script.js
@@ -1,3 +1,72 @@
+var product_details = [
+    {
+        name: "客製化陶瓷盤",
+        price: "(S) 280/(L) 300",
+        url: "https://myship.7-11.com.tw/general/detail/GM2411147095868",
+        description: "客製化陶瓷盤，可以根據您的需求進行設計和製作。<br>\
+        碗盤任搭滿三件折$100。",
+        spec:"尺寸：大：直徑 19cm，小：直徑 17cm",
+        img: "assets/product-1.jpg",
+    },
+    {
+        name: "客製化陶瓷碗",
+        price: "(S)250/(L) 280",
+        url: "https://myship.7-11.com.tw/general/detail/GM2411147095868",
+        description: "客製化陶瓷碗，可以根據您的需求進行設計和製作。<br>\
+        碗盤任搭滿三件折$100。",
+        spec:"尺寸：大：直徑 15cm，小：直徑 11.5cm",
+        img: "assets/product-2.png",
+    },
+    {
+        name: "客製化寵物高腳碗",
+        price: "580",
+        url: "https://myship.7-11.com.tw/general/detail/GM2411147095868",
+        description: "客製化寵物高腳碗，可以根據您的需求進行設計和製作。",
+        img: "assets/product-3.jpg",
+    },
+    {
+        name: "聖誕節限定客製化寵物高腳碗",
+        price: "620",
+        url: "https://myship.7-11.com.tw/general/detail/GM2411147095868",
+        description: "聖誕節限定客製化寵物高腳碗，可以根據您的需求進行設計和製作。",
+        img: "assets/product-4.jpg",
+    },
+    {
+        name: "客製化寵物似顏繪",
+        price: "100",
+        url: "https://myship.7-11.com.tw/general/detail/GM2411147095868",
+        description: "客製化寵物似顏繪，可以根據您的需求進行設計和製作。",
+        img: "assets/product-5.jpg",
+    },
+];
+
+function add_to_cart(index) {
+    window.location.href = product_details[index].url;
+}
+
+function see_details(index) {
+    // show a dialog with the product details
+    var dialog = document.createElement("dialog");
+    dialog.classList.add("product-dialog");
+    dialog.innerHTML = `
+    <h2>商品名稱：${product_details[index].name}</h2>
+    <p>價格：${product_details[index].price}</p>
+    <p>商品描述：${product_details[index].description}</p>
+    `;
+    if (product_details[index].spec) {
+        dialog.innerHTML += `<p>規格：${product_details[index].spec}</p>`;
+    }
+    document.body.appendChild(dialog);
+    var close_button = document.createElement("button");
+    close_button.classList.add("close-button");
+    close_button.innerHTML = "關閉";
+    close_button.onclick = function() {
+        dialog.close();
+    };
+    dialog.appendChild(close_button);
+    dialog.showModal();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // 獲取所有導航連結
     const navLinks = document.querySelectorAll('nav a');
@@ -99,16 +168,4 @@ class Carousel {
 document.addEventListener('DOMContentLoaded', () => {
     const carousels = document.querySelectorAll('.ad-carousel');
     carousels.forEach(carousel => new Carousel(carousel));
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    // 獲取所有加入購物車按鈕
-    const addToCartButtons = document.querySelectorAll('.product-card button');
-    
-    addToCartButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            // 跳轉到 7-11 賣貨便商品頁面
-            window.location.href = 'https://myship.7-11.com.tw/general/detail/GM2411147095868';
-        });
-    });
 });
